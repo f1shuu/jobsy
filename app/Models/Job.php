0e14d5b5d62c0add0e28de7f36347b216030c729
f1shu <szulzyckifilip@gmail.com>
2024-03-27 21:59:23 +0100

    Day 9: Update Job class

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,34 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if ($job) return $job;
-        else abort(404);
-    }
+    protected $fillable = ['title', 'salary'];
 }
