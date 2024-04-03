c39ddb7bc16544df8718914617357fce7e897a59
f1shu <szulzyckifilip@gmail.com>
2024-04-03 11:19:50 +0200

    Day 10: Create first factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
