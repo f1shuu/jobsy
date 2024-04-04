cf42225a5ff7303dfe3b8825d914904047891fb2
f1shu <szulzyckifilip@gmail.com>
2024-04-04 14:04:09 +0200

    Day 12: Implement a belongsToMany relationship

---
+++ database/migrations/2024_04_04_113503_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('job_tag');
+    }
+};
