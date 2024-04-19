656e4afc90ff05c639c3faef87e2fabf26d83baa
f1shu <szulzyckifilip@gmail.com>
2024-04-19 15:08:22 +0200

    Day 20 & 21: Create registration and login forms - part 1/2

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
