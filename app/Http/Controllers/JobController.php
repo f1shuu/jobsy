81bd53268f0d18de182a154de940bdefbb4fcc23
f1shu <szulzyckifilip@gmail.com>
2024-04-25 19:22:15 +0200

    Day 24: Add mailing system

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Mail\JobPosted;
 use App\Models\Job;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -35,11 +34,15 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
