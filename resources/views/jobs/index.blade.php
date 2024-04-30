f6f65dd2e33f720f4e5e883c589c19806b272b68
f1shu <szulzyckifilip@gmail.com>
2024-04-30 15:50:06 +0200

    Day 26: Prepare app for production

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,7 +8,7 @@
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-ray-200 rounded-lg">
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
                 <div>
-                    <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
+                    <strong class="text-jobsy">{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
